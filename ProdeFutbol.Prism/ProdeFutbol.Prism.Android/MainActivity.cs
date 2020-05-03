c03d4918cd89ddb3c92f54ade31994a943821821
Juan Manuel Ramirez <juanchiramirez5@gmail.com>
2020-05-03 16:58:34 -0300

    Add Splash and icon in Android

--- ProdeFutbol.Prism/ProdeFutbol.Prism.Android/MainActivity.cs
+++ ProdeFutbol.Prism/ProdeFutbol.Prism.Android/MainActivity.cs
@@ -7,7 +7,12 @@ using Syncfusion.SfBusyIndicator.XForms.Droid;
 
 namespace ProdeFutbol.Prism.Droid
 {
-    [Activity(Label = "ProdeFutbol.Prism", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(
+        Label = "Prode Mundial", 
+        Icon = "@mipmap/ic_launcher", 
+        Theme = "@style/MainTheme", 
+        MainLauncher = false, 
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
