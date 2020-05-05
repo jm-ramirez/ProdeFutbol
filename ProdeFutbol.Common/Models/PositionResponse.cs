cd83e964408ee8b23e17e5fc4170ddd92f14a882
Juan Manuel Ramirez <juanchiramirez5@gmail.com>
2020-05-05 19:37:17 -0300

    API get positions by tournament

---
+++ ProdeFutbol.Common/Models/PositionResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace ProdeFutbol.Common.Models
+{
+    public class PositionResponse
+    {
+        public UserResponse UserResponse { get; set; }
+
+        public int Points { get; set; }
+
+        public int Ranking { get; set; }
+    }
+}
