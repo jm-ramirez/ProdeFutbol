3d87737f58f523a0a185377dcbfda42818930d8d
Juan Manuel Ramirez <juanchiramirez5@gmail.com>
2020-05-10 21:09:01 -0300

    Revert "Revert "Imprve check connection""
    This reverts commit 306be5b23c88f76c443aa7d48c0fd814f03bd38c.

--- ProdeFutbol.Prism/ProdeFutbol.Prism/ViewModels/TournamentsPageViewModel.cs
+++ ProdeFutbol.Prism/ProdeFutbol.Prism/ViewModels/TournamentsPageViewModel.cs
@@ -38,8 +38,7 @@ namespace ProdeFutbol.Prism.ViewModels
         {
             IsRunning = true;
             string url = App.Current.Resources["UrlAPI"].ToString();
-            bool connection = await _apiService.CheckConnectionAsync(url);
-            if (!connection)
+            if (!_apiService.CheckConnection())
             {
                 IsRunning = false;
                 await App.Current.MainPage.DisplayAlert(Languages.Error, Languages.ConnectionError, Languages.Accept);
