b3a3a797194cadfd2dfff5a8c9baf645990afc61
Juan Manuel Ramirez <juanchiramirez5@gmail.com>
2020-05-10 21:23:06 -0300

    Revert "Login with Facebook App"
    This reverts commit 2a4fa1327356e5ddb5f7db258fbe001cb59a7c10.

--- ProdeFutbol.Prism/ProdeFutbol.Prism.Android/MainActivity.cs
+++ ProdeFutbol.Prism/ProdeFutbol.Prism.Android/MainActivity.cs
@@ -1,13 +1,9 @@
 ﻿using Android.App;
-using Android.Content;
 using Android.Content.PM;
 using Android.OS;
-using Java.Security;
-using Plugin.FacebookClient;
 using Prism;
 using Prism.Ioc;
 using Syncfusion.SfBusyIndicator.XForms.Droid;
-using System;
 
 namespace ProdeFutbol.Prism.Droid
 {
@@ -25,44 +21,14 @@ namespace ProdeFutbol.Prism.Droid
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(bundle);
-            FacebookClientManager.Initialize(this);
             /*CrossCurrentActivity.Current.Init(this, bundle);*/
             Xamarin.Essentials.Platform.Init(this, bundle);
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
             new SfBusyIndicatorRenderer();
             FFImageLoading.Forms.Platform.CachedImageRenderer.Init(true);
-            //GetAppHash();
             LoadApplication(new App(new AndroidInitializer()));
 
-        }
-        /*  ESTO ES PARA OBTENER EL HASH PARA FACEBOOK  RrTwYcj/tILpWFR9Aj8ZimD3PvY= 
-        private void GetAppHash()
-        {
-            try
-            {
-                PackageInfo info = Android.App.Application.Context.PackageManager.GetPackageInfo(Android.App.Application.Context.PackageName, PackageInfoFlags.Signatures);
-                foreach (var signature in info.Signatures)
-                {
-                    MessageDigest md = MessageDigest.GetInstance("SHA");
-                    md.Update(signature.ToByteArray());
-
-                    var hash = Convert.ToBase64String(md.Digest());
-                }
-            }
-            catch (NoSuchAlgorithmException e)
-            {
-                System.Diagnostics.Debug.WriteLine(e);
-            }
-            catch (Exception e)
-            {
-                System.Diagnostics.Debug.WriteLine(e);
-            }
-        }*/
-        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
-        {
-            base.OnActivityResult(requestCode, resultCode, data);
-            FacebookClientManager.OnActivityResult(requestCode, resultCode, data);
         }
 
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Android.Content.PM.Permission[] grantResults)
@@ -71,7 +37,6 @@ namespace ProdeFutbol.Prism.Droid
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
-        
     }
 
     public class AndroidInitializer : IPlatformInitializer
